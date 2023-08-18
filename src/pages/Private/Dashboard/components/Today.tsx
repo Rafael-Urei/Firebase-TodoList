2f99af8ce3d9454f69ce88f8b25e8af2b7ba5824
Rafael-Urei <urei.dev@outlook.com>
2023-08-18 14:29:43 -0300

    Comentei algumas partes do código.

--- src/pages/Private/Dashboard/components/Today.tsx
+++ src/pages/Private/Dashboard/components/Today.tsx
@@ -1,16 +1,16 @@
-import { ArrowRight } from "lucide-react";
-import { tasks } from "../../../../shared/contexts/TasksContext/TasksContext";
+// import { ArrowRight } from "lucide-react";
+// import { tasks } from "../../../../shared/contexts/TasksContext/TasksContext";
 import { AddTaskButton } from "../../../../shared/components/Buttons/AddTaskButton/AddTaskButton";
 
 export const TodayTasks = () => {
-  const actualDate = new Date().getDate();
+  // const actualDate = new Date().getDate();
   return (
     <>
       <div className="flex bg-zinc-50 border rounded-md p-5 flex-col gap-4 w-full">
         <h1 className="text-lg font-bold text-zinc-700">Today</h1>
         <AddTaskButton />
         <ul>
-          {tasks.map((task) => {
+          {/* {tasks.map((task) => {
             if (task.date === actualDate) {
               return (
                 <div
@@ -29,7 +29,7 @@ export const TodayTasks = () => {
             } else {
               return;
             }
-          })}
+          })} */}
         </ul>
       </div>
     </>
--- src/pages/Private/Dashboard/components/Tomorrow.tsx
+++ src/pages/Private/Dashboard/components/Tomorrow.tsx
@@ -1,16 +1,16 @@
-import { ArrowRight } from "lucide-react";
-import { tasks } from "../../../../shared/contexts/TasksContext/TasksContext";
+// import { ArrowRight } from "lucide-react";
+// import { tasks } from "../../../../shared/contexts/TasksContext/TasksContext";
 import { AddTaskButton } from "../../../../shared/components/Buttons/AddTaskButton/AddTaskButton";
 
 export const TomorrowTasks = () => {
-  const actualDate = new Date().getDate();
+  // const actualDate = new Date().getDate();
   return (
     <>
       <div className="flex bg-zinc-50 border rounded-md p-5 flex-col gap-4 w-full">
         <h1 className="text-lg font-bold text-zinc-700">Tomorrow</h1>
         <AddTaskButton />
         <ul>
-          {tasks.map((task) => {
+          {/* {tasks.map((task) => {
             if (task.date === actualDate + 1) {
               return (
                 <div
@@ -29,7 +29,7 @@ export const TomorrowTasks = () => {
             } else {
               return;
             }
-          })}
+          })} */}
         </ul>
       </div>
     </>
